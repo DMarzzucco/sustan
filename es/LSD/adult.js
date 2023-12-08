411a1880f2106f85e6f540d2c419639c0584c5cb
DMarzzucco <drrkermazyxokv@gmail.com>
2023-12-08 19:58:09 -0300

    update

---
+++ es/LSD/adult.js
@@ -0,0 +1,9 @@
+// tabla
+function openTab(tabName){
+    var tablas=document.querySelectorAll('.tab');
+    tablas.forEach(function(tab){
+        tab.classList.remove('active', 'scale-up-center');
+    });
+
+    document.getElementById(tabName).classList.add('active', 'scale-up-center');   
+}
