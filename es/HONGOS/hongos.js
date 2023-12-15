9605415d41f5e1bf85fc0ece4a7dec57355d9869
DMarzzucco <drrkermazyxokv@gmail.com>
2023-12-15 14:10:07 -0300

    update

--- es/HONGOS/hongos.js
+++ es/HONGOS/hongos.js
@@ -1,8 +1,8 @@
 function openTab(tabName){
     var tablas=document.querySelectorAll('.tab');
     tablas.forEach(function(tab){
-        tab.classList.remove('active', 'scale-up-center');
+        tab.classList.remove('active', 'escala-up');
     });
 
-    document.getElementById(tabName).classList.add('active', 'scale-up-center');   
+    document.getElementById(tabName).classList.add('active', 'escala-up');   
 }
